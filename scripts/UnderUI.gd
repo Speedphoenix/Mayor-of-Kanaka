f2536bea5572fdefc49aff81e6ccb1f95c46e100
Billette <maxime.billette@gmail.com>
2020-12-01 11:15:47 +0100

    improve event log system

--- scripts/UnderUI.gd
+++ scripts/UnderUI.gd
@@ -29,39 +29,65 @@ func _on_EventDetailsButton_toggled(button_pressed):
 		var eventCounter = 0
 		for event in eventsList:
 			eventCounter += 1
-			var eventTitle = event["title"]
-			var eventID = event["id"]
-			var detailMenuController = $EventDetailsController/DetailsMenuController
-			
-			var controller = Control.new()
-			controller.name = "Controller" + String(eventCounter)
-			controller.margin_left = 40
-			controller.margin_top = 100 + (eventCounter * 45)
-			
-			var background = ColorRect.new()
-			background.name = "background" + String(eventCounter)
-			background.margin_right = 175
-			background.margin_bottom = 35
-			background.color = Color("531919")
-			
-			var button = Button.new()
-			background.name = "button" + String(eventCounter)
-			button.flat = true
-			button.text = eventTitle
-			button.margin_right = 175
-			button.margin_bottom = 35
-			
-			button.connect("pressed", self, "_on_eventButton_pressed", [eventID])
-			
-			controller.add_child(background)
-			controller.add_child(button)
-			detailMenuController.add_child(controller)
+			if eventCounter <= 5 :
+				var eventTitle = event["title"]
+				var eventID = event["id"]
+				var detailMenuController = $EventDetailsController/DetailsMenuController
+				
+				var controller = Control.new()
+				controller.name = "Controller" + String(eventCounter)
+				controller.margin_left = 40
+				controller.margin_top = 100 + (eventCounter * 45)
+				
+				var background = ColorRect.new()
+				background.name = "background" + String(eventCounter)
+				background.margin_right = 175
+				background.margin_bottom = 35
+				background.color = Color("531919")
+				
+				var button = Button.new()
+				background.name = "button" + String(eventCounter)
+				button.flat = true
+				button.text = eventTitle
+				button.margin_right = 175
+				button.margin_bottom = 35
+				
+				button.connect("pressed", self, "_on_eventButton_pressed", [eventID])
+				
+				controller.add_child(background)
+				controller.add_child(button)
+				detailMenuController.add_child(controller)
 			
 		$EventDetailsController/DetailsMenuController.show()
 	else:
+		var detailMenuController = $EventDetailsController/DetailsMenuController
+		var background = $EventDetailsController/DetailsMenuController/Background
+		var backgroundBorder = $EventDetailsController/DetailsMenuController/BackgroundBorder
+		for n in detailMenuController.get_children():
+			if (n != background && n != backgroundBorder):
+				detailMenuController.remove_child(n)
+				n.queue_free()
 		$EventDetailsController/DetailsMenuController.hide()
 
 
 func _on_eventButton_pressed(eventID):
-	#print("Event ID to be printed : ", eventID)
+	#print("Event ID to be displayed : ", eventID)
 	emit_signal("eventToDisplay", eventID)
+
+
+func _on_event_accepted(event):
+	var Global = get_node("/root/Global")
+	eventsList = Global.eventsList
+	#var eventID = event["id"]
+	eventsList.erase(event)
+	Global.eventsList = eventsList
+	$EventDetailsController/DetailsMenuController.hide()
+
+
+func _on_event_refused(event):
+	var Global = get_node("/root/Global")
+	eventsList = Global.eventsList
+	#var eventID = event["id"]
+	eventsList.erase(event)
+	Global.eventsList = eventsList
+	$EventDetailsController/DetailsMenuController.hide()
