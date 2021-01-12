90d09824a40b4df1c89524fa0ee01d563a11c6cc
Speedphoenix <leonardo.jeanteur@gmail.com>
2021-01-13 00:48:23 +0100

    Move taxi cab iterator to own script

--- scripts/CityMap.gd
+++ scripts/CityMap.gd
@@ -30,70 +30,7 @@ onready var turn_controller = global.get_node("TurnController")
 
 onready var tileid_background_filled := tile_set.find_tile_by_name(tilename_background_filled_tile)
 
-
 # An iterator to go through each cell in order of manhattan distance
-class TaxiCabRange:
-	var curr_distance: int
-	var max_distance: int
-	var start_distance: int
-	var stage: int
-	var x: int
-	var y: int
-	
-	enum Stages {RIGHT_TO_TOP = 0, TOP_TO_LEFT = 1, LEFT_TO_BOTTOM = 2, BOTTOM_TO_RIGHT = 3}
-	
-	# Use -1 to not set any maximum distance
-	# max_distance is included in the iteration
-	func _init(max_distance := -1, start_distance := 0):
-		self.max_distance = max_distance
-		self.start_distance = start_distance
-
-	func should_continue() -> bool:
-		return max_distance == -1 || curr_distance <= max_distance
-
-	func _distance_init(dist: int):
-		curr_distance = dist
-		if dist == 0:
-			x = 0
-			y = 0
-			# The last stage, so the next iteration will increase distance
-			stage = Stages.BOTTOM_TO_RIGHT
-		else:
-			x = dist
-			y = 0
-			# The first stage
-			stage = Stages.RIGHT_TO_TOP
-
-	func _iter_init(_arg) -> bool:
-		_distance_init(start_distance)
-		return should_continue()
-
-	func _iter_next(_arg) -> bool:
-		# Can only happen on first iteration if start_distance = 0
-		if (x == 0 && y == 0):
-			_distance_init(curr_distance + 1)
-			return should_continue()
-		match stage:
-			Stages.RIGHT_TO_TOP:
-				x -= 1
-				y += 1
-			Stages.TOP_TO_LEFT:
-				x -= 1
-				y -= 1
-			Stages.LEFT_TO_BOTTOM:
-				x += 1
-				y -= 1
-			Stages.BOTTOM_TO_RIGHT:
-				x += 1
-				y += 1
-		if (x == 0 && (stage % 2 == 0)) || (y == 0 && (stage % 2 == 1)):
-			stage += 1
-		if stage >= 4:
-			_distance_init(curr_distance + 1)
-		return should_continue()
-
-	func _iter_get(_arg) -> Vector2:
-		return Vector2(x, y)
 
 func _ready():
 	assert(tile_set == foreground_city.tile_set && tile_set == background_city.tile_set)
@@ -136,7 +73,7 @@ func _spot_is_available(pos: Vector2, dims: Vector2) -> bool:
 # search_limit is inclusive and decides how far to look in terms of Manhattan distance
 func _get_available_spot(dims: Vector2, around_where: Vector2, count: int = 1, search_limit: int = 5) -> Array:
 	var rep := []
-	for diff in TaxiCabRange.new(search_limit):
+	for diff in TaxiCabIterator.new(search_limit):
 		var curr_cell: Vector2 = around_where + diff
 		var spot_origin: Vector2
 		# Please tell me if you have a prettier way of doing this
---
+++ scripts/utils/TaxiCabIterator.gd
@@ -0,0 +1,65 @@
+class_name TaxiCabIterator
+extends Reference
+
+# This class is currently rather unused 
+var curr_distance: int
+var max_distance: int
+var start_distance: int
+var stage: int
+var x: int
+var y: int
+
+enum Stages {RIGHT_TO_TOP = 0, TOP_TO_LEFT = 1, LEFT_TO_BOTTOM = 2, BOTTOM_TO_RIGHT = 3}
+
+# Use -1 to not set any maximum distance
+# max_distance is included in the iteration
+func _init(max_distance := -1, start_distance := 0):
+	self.max_distance = max_distance
+	self.start_distance = start_distance
+
+func should_continue() -> bool:
+	return max_distance == -1 || curr_distance <= max_distance
+
+func _distance_init(dist: int):
+	curr_distance = dist
+	if dist == 0:
+		x = 0
+		y = 0
+		# The last stage, so the next iteration will increase distance
+		stage = Stages.BOTTOM_TO_RIGHT
+	else:
+		x = dist
+		y = 0
+		# The first stage
+		stage = Stages.RIGHT_TO_TOP
+
+func _iter_init(_arg) -> bool:
+	_distance_init(start_distance)
+	return should_continue()
+
+func _iter_next(_arg) -> bool:
+	# Can only happen on first iteration if start_distance = 0
+	if (x == 0 && y == 0):
+		_distance_init(curr_distance + 1)
+		return should_continue()
+	match stage:
+		Stages.RIGHT_TO_TOP:
+			x -= 1
+			y += 1
+		Stages.TOP_TO_LEFT:
+			x -= 1
+			y -= 1
+		Stages.LEFT_TO_BOTTOM:
+			x += 1
+			y -= 1
+		Stages.BOTTOM_TO_RIGHT:
+			x += 1
+			y += 1
+	if (x == 0 && (stage % 2 == 0)) || (y == 0 && (stage % 2 == 1)):
+		stage += 1
+	if stage >= 4:
+		_distance_init(curr_distance + 1)
+	return should_continue()
+
+func _iter_get(_arg) -> Vector2:
+	return Vector2(x, y)
