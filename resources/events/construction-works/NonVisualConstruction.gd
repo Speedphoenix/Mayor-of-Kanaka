b852f45161483f697d1305ae1e5ed35194cbf75f
lazovchik <lazovchik@gmail.com>
2021-01-13 15:50:34 +0100

    minor changes to debug

--- resources/events/construction-works/NonVisualConstruction.gd
+++ resources/events/construction-works/NonVisualConstruction.gd
@@ -6,8 +6,10 @@ extends DecisionSimple
 #will generate a random number 
 var rng = RandomNumberGenerator.new()
 
-var event_title_description = [['Water supply system'], ['Water filtering system'],
-	['Electricity delivery system'], ['Electricity consumption policies']]
+var event_title_description = [['Water supply system', 'The system is used'], 
+	['Water filtering system', 'The system is used'], 
+	['Electricity delivery system', 'The system is used'], 
+	['Electricity consumption policies', 'Policies are too old.']]
 
 func _init():
 	#on accept
@@ -30,9 +32,11 @@ func _init():
 func on_triggered(scene_tree: SceneTree) -> void:
 	.on_triggered(scene_tree)
 	#ex: Hurricane Incomming !!!
-	title = trouble_name[rng.randi_range(0,trouble_name.size()-1)] + ' Incomming!!!'
+	title = event_title_description[rng.randi_range(0,event_title_description.size()-1)][0] + '.'
 	#Amount of money to spent if we want to softeh crisis concsequences
-	var work_cost = rng.randi_range(-100,-20)
+	var improvement_monthly_cost = rng.randi_range(-50,-15)
+	var ecological_benefit = rng.randi_range(2,3)
+	var satisfaction_benefit = rng.randi_range(1,3)
 	
 	description = ('Mayor, a troubble has occured on ' 
 		+ street_name[rng.randi_range(0,street_name.size()-1)] + ' Street.'
