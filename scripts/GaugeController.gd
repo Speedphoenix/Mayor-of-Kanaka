2becd352304a74336741a4270399ed589ce39312
Speedphoenix <leonardo.jeanteur@gmail.com>
2021-01-14 03:59:58 +0100

    Add gauge creation and make gauges floats
    floats instead of ints

--- scripts/GaugeController.gd
+++ scripts/GaugeController.gd
@@ -29,41 +29,32 @@ var _gauges: Dictionary = {
 	"BUDGET": 50,
 }
 
+
 static func get_gauge_controller(scene_tree: SceneTree) -> GaugeController:
 	# Not using GlobalObject.get_global_object because cyclic reference
 	return scene_tree.get_current_scene().get_node("GlobalObject/GaugeController") as GaugeController
 
-func _gauges_are_different(gauges1: Dictionary, gauges2: Dictionary):
-	for el in gauges1:
-		if gauges1[el] != gauges2[el]:
-			return true
-	return false
-
-func _apply_limits(name: String) -> int:
-	if gauge_limits.has(name):
-		if gauge_limits[name].has("LOWER") && _gauges[name] < gauge_limits[name].LOWER:
-			_gauges[name] = gauge_limits[name].LOWER
-		if gauge_limits[name].has("UPPER") && _gauges[name] > gauge_limits[name].UPPER:
-			_gauges[name] = gauge_limits[name].UPPER
-	return _gauges[name]
+func create_gauge(name: String, initial_value: float = 0, limits := {}):
+	_gauges[name] = initial_value
+	if !limits.empty():
+		_set_gauge_limits(name, limits)
+	emit_signal("gauge_created", name, initial_value)
 
 # limits should contain either "LOWER", "UPPER" or both
+# Will emit a signal if the value of the gauge changed
 func set_gauge_limits(name: String, limits: Dictionary) -> void:
-	if !(name in gauge_limits):
-		gauge_limits[name] = {}
-	if "LOWER" in limits:
-		gauge_limits[name]["LOWER"] = limits["LOWER"]
-	if "UPPER" in limits:
-		gauge_limits[name]["UPPER"] = limits["UPPER"]
-	if "LOWER" in limits && "UPPER" in limits:
-		assert(gauge_limits[name]["UPPER"] >= gauge_limits[name]["LOWER"])
+	var old_value: float = _gauges[name]
+	_set_gauge_limits(name, limits)
+	var new_value: float = _gauges[name]
+	if old_value != new_value:
+		emit_signal("gauge_changed", name, new_value, old_value)
 
 # limits should be a dictionary of the same type as gauge_limits
 func set_gauges_limits(limits: Dictionary):
 	for name in limits:
 		set_gauge_limits(name, limits[name])
 
-func get_gauge(name: String) -> int:
+func get_gauge(name: String) -> float:
 	assert(name in _gauges)
 	return _gauges[name]
 
@@ -74,7 +65,7 @@ func get_gauges() -> Dictionary:
 	return _gauges.duplicate()
 
 # Sets gauge and emits the signal
-func set_gauge(name: String, value: int) -> void:
+func set_gauge(name: String, value: float) -> void:
 	assert(name in _gauges)
 	var old_value = _gauges[name]
 	_gauges[name] = value
@@ -82,7 +73,7 @@ func set_gauge(name: String, value: int) -> void:
 	if emit_signal_on_identical_new_value || old_value != new_value:
 		emit_signal("gauge_changed", name, new_value, old_value)
 
-func apply_to_gauge(name: String, diff: int) -> void:
+func apply_to_gauge(name: String, diff: float) -> void:
 	assert(name in _gauges)
 	set_gauge(name, _gauges[name] + diff)
 
@@ -93,3 +84,31 @@ func set_gauges(values: Dictionary) -> void:
 func apply_to_gauges(differences: Dictionary) -> void:
 	for gauge_name in differences:
 		apply_to_gauge(gauge_name, differences[gauge_name])
+
+# limits should contain either "LOWER", "UPPER" or both
+func _set_gauge_limits(name: String, limits: Dictionary) -> void:
+	if !(name in gauge_limits):
+		gauge_limits[name] = {}
+	if "LOWER" in limits:
+		gauge_limits[name]["LOWER"] = limits["LOWER"]
+	if "UPPER" in limits:
+		gauge_limits[name]["UPPER"] = limits["UPPER"]
+	if "LOWER" in limits && "UPPER" in limits:
+		assert(gauge_limits[name]["UPPER"] >= gauge_limits[name]["LOWER"])
+		gauge_limits[name]["LOWER"] = gauge_limits[name]["UPPER"]
+		# assert only kills the execution in debug builds
+	_apply_limits(name)
+
+func _gauges_are_different(gauges1: Dictionary, gauges2: Dictionary):
+	for el in gauges1:
+		if gauges1[el] != gauges2[el]:
+			return true
+	return false
+
+func _apply_limits(name: String) -> float:
+	if gauge_limits.has(name):
+		if gauge_limits[name].has("LOWER") && _gauges[name] < gauge_limits[name].LOWER:
+			_gauges[name] = gauge_limits[name].LOWER
+		if gauge_limits[name].has("UPPER") && _gauges[name] > gauge_limits[name].UPPER:
+			_gauges[name] = gauge_limits[name].UPPER
+	return _gauges[name]
