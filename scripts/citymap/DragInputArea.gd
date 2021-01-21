bebf172a8f7ff4232fae1f1b4a001e7a5db272d2
Speedphoenix <leonardo.jeanteur@gmail.com>
2021-01-21 05:45:10 +0100

    Make the drag area dimensions dynamic, to work with any viewport dims

--- scripts/citymap/DragInputArea.gd
+++ scripts/citymap/DragInputArea.gd
@@ -7,8 +7,18 @@ signal map_dragged(difference)
 var hovering := false
 var clicking := false
 
+func _ready():
+	get_tree().get_root().connect("size_changed", self, "update_drag_area")
+	update_drag_area()
+
 func move_map(diff: Vector2):
 	emit_signal("map_dragged", diff)
+
+func update_drag_area():
+	var viewport_size: Vector2 = get_viewport_rect().size
+
+	$FullViewportShape.position = viewport_size / 2
+	($FullViewportShape.shape as RectangleShape2D).extents = viewport_size / 2
 
 func _input(event: InputEvent):
 	if (
