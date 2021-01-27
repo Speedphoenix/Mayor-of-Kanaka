621264b2e710c7aaecc770bf2baf05c882ac07dc
SarahL24 <36310493+SarahL24@users.noreply.github.com>
2021-01-27 23:45:48 +0100

    Create newgames parameters
    Create game parameters for the 3 buttons
    Scene NewGame : export the variables (bars) to be able to modified them

--- scripts/menu/GameMenu.gd
+++ scripts/menu/GameMenu.gd
@@ -1,17 +1,16 @@
 extends Control
 
-const mainscene = preload("res://scenes/MainScene.tscn")
-
+export(PackedScene) var options_menu = preload("res://scenes/menu/OptionMenu.tscn")
 
 func _on_Options_pressed():
-	var option_menu = load("res://scenes/menu/OptionMenu.tscn").instance()
+	var option_menu = options_menu.instance()
 	add_child(option_menu)
 	get_node("OptionMenu").connect("CloseOptionsMenu", self, "CloseOptionsMenu")
 	var language_bar_option = get_node("OptionMenu/HBoxContainer/Colum1/languageBar")
 	language_bar_option.add_item("English")
 	get_tree().paused = true
-	#get_tree().change_scene("res://scenes/menu/OptionMenu.tscn")
-
+	
+	
 func CloseOptionsMenu():
 	get_node("OptionMenu").queue_free()
 	get_tree().paused = false
--- scripts/menu/NewGameButton.gd
+++ scripts/menu/NewGameButton.gd
@@ -1,9 +1,19 @@
 extends TextureButton
 
 export(Resource) var game_parameters: Resource = null
+
 onready var global_game_info: GlobalGameInfo = get_node("/root/GlobalGameInfo")
 
+export (String ) var title
+export (String ) var budget
+export (String ) var health
+export (String ) var nature
+export (String ) var satisfaction
+export (String ) var mayor
+
 func _ready():
+	$GameTitle.text = title
+	$GameInfos.text = "Budget: "+budget+ "M$\nHealth: "+ health + "\nEcology: "+ nature + "\nCitizen satisfaction: " + satisfaction + "\nMayor Stress: " +mayor 
 	assert(game_parameters is GameParameters or game_parameters == null)
 
 func _pressed():
