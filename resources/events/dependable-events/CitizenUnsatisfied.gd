1f99ed8ad0b092177c1bffa948f6688a67f18c65
lazovchik <lazovchik@gmail.com>
2021-01-28 16:03:47 +0100

    Changed all dependable events
    - adjusted gauges
    - changed random fcts tp WeigthChoice fcts

--- resources/events/dependable-events/CitizenUnsatisfied.gd
+++ resources/events/dependable-events/CitizenUnsatisfied.gd
@@ -7,16 +7,16 @@ func _init():
 	#Mayor will do a public speach
 	accept_effects = {
 		"on_gauges": {
-			"BUDGET": rng.randi_range(-300, -100),
-			"SATISFACTION": rng.randi_range(10, 15),
-			"STRESS": rng.randi_range(-15, -10),
+			"BUDGET": WeightChoice.randi_range(-30, -10),
+			"SATISFACTION": WeightChoice.randi_range(7, 12),
+			"STRESS": WeightChoice.randi_range(-10, -5),
 			},
 	}
 	#on decline
 	#City hall does not react, satisfaction goes down, stress increrase
 	refuse_or_expire_effects = {
 		"on_gauges": {
-		"SATISFACTION": rng.randi_range(-10, -5),
-		"STRESS": rng.randi_range(5, 10),
+		"SATISFACTION": WeightChoice.randi_range(-10, -5),
+		"STRESS": WeightChoice.randi_range(5, 7),
 		},
 	}
