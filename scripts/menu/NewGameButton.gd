998e089a5ea8c594956310c7aec50b17952931ac
SarahL24 <36310493+SarahL24@users.noreply.github.com>
2021-01-29 14:39:37 +0100

    Add a game in MainMenu
    Add a game in GameMenu
    Retrieve initial gauges of gameparameters for newbuttons
    change gameparameters of newgames

--- scripts/menu/NewGameButton.gd
+++ scripts/menu/NewGameButton.gd
@@ -5,16 +5,17 @@ export(Resource) var game_parameters: Resource = null
 onready var global_game_info: GlobalGameInfo = get_node("/root/GlobalGameInfo")
 
 export (String) var title
-export (String) var budget
-export (String) var health
-export (String) var nature
-export (String) var satisfaction
-export (String) var mayor
+
 
 func _ready():
+	var budget = str(stepify(game_parameters.initial_gauges["BUDGET"], 1))
+	var health = str(stepify(game_parameters.initial_gauges["HEALTH"], 0.1))
+	var satisfaction = str(stepify(game_parameters.initial_gauges["SATISFACTION"], 0.1))
+	var stress = str(stepify(game_parameters.initial_gauges["STRESS"], 0.1))
+	var nature = str(stepify(game_parameters.initial_gauges["NATURE"], 0.1))
 	$GameTitle.text = title
-	$GameInfos.text = ("Budget: " + budget + "M$\nHealth: " + health + "\nEcology: "
-			+ nature + "\nCitizen satisfaction: " + satisfaction + "\nMayor Stress: " + mayor)
+	$GameInfos.text = ("Budget: " + budget + "K$\nHealth: " + health + "\nEcology: "
+			+ nature + "\nCitizen satisfaction: " + satisfaction + "\nMayor Stress: " + stress)
 	assert(game_parameters is GameParameters or game_parameters == null)
 
 func _pressed():
