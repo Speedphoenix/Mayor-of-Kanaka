4bdc165949ebee49c5a14d34fddaceff4dc07334
Speedphoenix <leonardo.jeanteur@gmail.com>
2021-02-01 16:55:12 +0100

    fixup! Add factory, church and small park tiles

--- scripts/events/effects/PopulationEffect.gd
+++ scripts/events/effects/PopulationEffect.gd
@@ -1,6 +1,8 @@
 class_name PopulationEffect
 extends BaseEffect
 
+export(int) var house_count := 1
+
 # Called when the event is created, before the player has interracted with it
 func on_triggered(_scene_tree: SceneTree, _event: BaseEvent) -> void:
 	pass
@@ -13,17 +15,9 @@ func on_resolved(scene_tree: SceneTree, resolve_type: int) -> void:
 	var population_controller := PopulationController.get_instance(scene_tree)
 	var turn_controller := TurnController.get_instance(scene_tree)
 	var remaining_days: int = turn_controller.days_in_a_month - turn_controller.current_miniturn_no
-
-func apply_effect(_scene_tree: SceneTree, resolve_type: int, _turn_delay: int) -> void:
-	pass
-
-# Returns false if this should not be accepted
-func blocks_accept(_scene_tree: SceneTree) -> bool:
-	return false
-
-func _set_delays(value: int):
-	delay = value
-
-# This can be overridden if need be
-func _get_delays() -> int:
-	return delay
+	var house_delay = 0
+	if delay > 0:
+		house_delay += remaining_days
+		if delay > 1:
+			house_delay += (delay - 1) * turn_controller.days_in_a_month
+	population_controller.add_random_houses(house_count, house_delay)
