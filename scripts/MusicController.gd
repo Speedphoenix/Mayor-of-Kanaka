18bffd642da6ee2cb4606f1479555709d8ea5b4c
Speedphoenix <leonardo.jeanteur@gmail.com>
2021-02-02 12:44:00 +0100

    Reduce the vertical size of the UI
    Also add exported variables for volume control
    Also add triggered signal and was_triggered property to BaseEffect

--- scripts/MusicController.gd
+++ scripts/MusicController.gd
@@ -1,6 +1,9 @@
 class_name MusicController
 extends Node
 
+export(float) var main_volume := -20.0
+export(float) var reduced_volume := -35.0
+
 onready var turn_controller := TurnController.get_instance(get_tree())
 
 static func get_instance(scene_tree: SceneTree) -> MusicController:
@@ -10,20 +13,20 @@ static func get_instance(scene_tree: SceneTree) -> MusicController:
 func _ready():
 	turn_controller.connect("turn_changed", self, "_on_turn_changed")
 
-func play_main_music():
+func play_main_music() -> void:
 	$MainMusic.play()
 	
-func play_event_sound_effect():
+func play_event_sound_effect() -> void:
 	$EventSoundEffect.play()
 	
-func play_turn_sound_effect():
+func play_turn_sound_effect() -> void:
 	$TurnSoundEffect.play()
 	
-func reduce_volume():
-	$MainMusic.volume_db = -35.0
+func reduce_volume() -> void:
+	$MainMusic.volume_db = reduced_volume
 
-func normal_volume():
-	$MainMusic.volume_db = -20.0
+func normal_volume() -> void:
+	$MainMusic.volume_db = main_volume
 
 func _on_turn_changed(_turn_number, _miniturn_number):
 	play_turn_sound_effect()
--- scripts/events/effects/BaseEffect.gd
+++ scripts/events/effects/BaseEffect.gd
@@ -1,6 +1,9 @@
 class_name BaseEffect
 extends Resource
 
+# Emitted once when it has been triggered
+signal triggered
+
 enum ResolveType {
 	ACCEPT,
 	REFUSE,
@@ -11,6 +14,8 @@ enum ResolveType {
 export(int) var delay := 1 setget _set_delays, _get_delays
 
 export(Array, ResolveType) var which_resolves := [ResolveType.ACCEPT]
+
+var was_triggered := false
 
 # Called when the event is created, before the player has interracted with it
 func on_triggered(_scene_tree: SceneTree, _event: BaseEvent) -> void:
@@ -23,6 +28,11 @@ func on_resolved(_scene_tree: SceneTree, _resolve_type: int) -> void:
 
 func apply_effect(_scene_tree: SceneTree, _resolve_type: int, _turn_delay: int) -> void:
 	pass
+
+# Call this from child classes to signal that the initialization sequece of the effect is over
+func set_as_triggered() -> void:
+	was_triggered = true
+	emit_signal("triggered")
 
 # Returns false if this should not be accepted
 func blocks_accept(_scene_tree: SceneTree) -> bool:
