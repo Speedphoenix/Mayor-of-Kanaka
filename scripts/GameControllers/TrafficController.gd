95741282a89cb650e568d2d980b962d14676faaa
Speedphoenix <leonardo.jeanteur@gmail.com>
2021-02-03 18:17:28 +0100

    Create TrafficController.add_traffic_element()
    The add_traffic_element function will add a car or some other traffic
    element to the map. It can be called from outside the TrafficController
    with a PackedScene (the root node of which should be or extend
    `MovingTraffic`)

--- scripts/GameControllers/TrafficController.gd
+++ scripts/GameControllers/TrafficController.gd
@@ -1,30 +1,41 @@
+class_name TrafficController
 extends Node
 
-export(PackedScene) var car_scene
+export(Array, PackedScene) var car_scenes := []
 
 export(float) var min_roads_per_car := 10.0
 export(int) var min_population_per_car := 10
 
-var current_cars := []
+var current_vehicles := []
 
 onready var gauge_controller := GaugeController.get_instance(get_tree())
 onready var city_map := CityMap.get_instance(get_tree())
 
+static func get_instance(scene_tree: SceneTree) -> TrafficController:
+	return scene_tree.get_current_scene().get_node("GlobalObject/GameControllers/TrafficController") as TrafficController
+
 func _ready():
-	assert(car_scene.can_instance())
+	assert(car_scenes.size() != 0)
+	for el in car_scenes:
+		assert(el.can_instance())
 	gauge_controller.connect("gauge_changed", self, "_on_gauge_changed")
+
+# Using force = false will respect the min_roads_per_car ratio
+func add_traffic_element(what: PackedScene, force := true) -> void:
+	var possible_roads: Array = city_map.get_drivable_roads()
+	var current_car_count = current_vehicles.size() if current_vehicles.size() >= 1 else 1
+	if !force && float(possible_roads.size()) / current_car_count < min_roads_per_car:
+		return
+	var instanced = what.instance()
+	city_map.traffic_layer.add_child(instanced)
+	instanced.initialize(WeightChoice.choose_random_from_array(possible_roads))
+	current_vehicles.append(instanced)
 
 func _on_gauge_changed(gauge_name: String, new_value: float, _old_value):
 	if gauge_name != "POPULATION":
 		return
-	var current_car_count = current_cars.size() if current_cars.size() >= 1 else 1
+	var current_car_count = current_vehicles.size() if current_vehicles.size() >= 1 else 1
 	var population_car_ratio: float = new_value / current_car_count
 	if population_car_ratio < min_population_per_car:
 		return
-	var possible_roads: Array = city_map.get_drivable_roads()
-	if float(possible_roads.size()) / current_car_count < min_roads_per_car:
-		return
-	var new_car: Node2D = car_scene.instance()
-	city_map.traffic_layer.add_child(new_car)
-	new_car.initialize(WeightChoice.choose_random_from_array(possible_roads))
-	current_cars.append(new_car)
+	add_traffic_element(WeightChoice.choose_random_from_array(car_scenes), false)
